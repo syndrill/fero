144434adc5002858971a18f4adbea449f9475c0c
zeroload <zeroload@mail.ru>
2019-01-20 13:04:41 +0700

    plugins: push loaded plugins into app context

--- fero/plugins/__init__.py
+++ fero/plugins/__init__.py
@@ -2,12 +2,16 @@ import glob
 import os
 import importlib
 
+from flask import g
+
 def init_plugins(app):
+    app._loaded_plugins = []
     modules = sorted(glob.glob(os.path.dirname(__file__) + "/*"))
     for module in modules:
         module_name = os.path.basename(module)
         if os.path.isdir(module) and module_name:
+            app._loaded_plugins.append(module_name)
             module = '.' + module_name
             module = importlib.import_module(module, package='fero.plugins')
             module.load(app)
-            app.logger.info("Loaded plugins: %s", module_name)
+    app.logger.info("Loaded plugins: %s", app._loaded_plugins)
